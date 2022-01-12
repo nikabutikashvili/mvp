e121a2e8a020178c16bde2900aecd09813b2dd88
Nika Butikashvili <butikashvilinika@gmail.com>
2022-01-12 09:46:38 +0100

    Add laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', \App\Http\Controllers\WelcomeController::class);
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
