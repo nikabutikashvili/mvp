1027f6c5ab50481e6cf1a7f0561c84f60cc914cb
Nika Butikashvili <butikashvilinika@gmail.com>
2022-01-14 00:22:04 +0100

    Add Product's Modal, Seeder, Factory and Migration

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->words(3, true),
+            'description' => $this->faker->realText(200),
+            'price' => $this->faker->randomFloat(2, 10, 100),
+        ];
+    }
+}
