8d81e5a0b8cbd1a0e9c88d20d7cca7987c594ce8
Nika Butikashvili <butikashvilinika@gmail.com>
2022-01-21 11:25:53 +0100

    Add cache to productCatalogue

--- app/Http/Controllers/ProductsCatalogueController.php
+++ app/Http/Controllers/ProductsCatalogueController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Http\Request;
 
 class ProductsCatalogueController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $products = Product::all();
+        $products = Cache::remember('products', 10, function () {
+            return Product::all();
+        });
+
         return view('product-catalogue', [
             'products' => $products
         ]);
